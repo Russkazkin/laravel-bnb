a205c2ecbb339c7f4381cdae9d53f923011897e1
Russkazkin <ruslan@skazkin.su>
2020-06-11 10:17:31 +0300

    Add couple vue components

---
+++ resources/js/bookables/BookableListItem.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <h1>Title</h1>
+    <p>Ut leo. Maecenas egestas arcu quis ligula mattis placerat. Fusce a quam. Quisque malesuada placerat nisl.
+        Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo.
+    </p>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "BookableListItem"
+    }
+</script>
+
+<style scoped>
+
+</style>
