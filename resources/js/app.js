dbe039386c35b0318e2ea343e8abfd70ba3cabd5
Russkazkin <ruslan@skazkin.su>
2020-06-11 09:15:01 +0300

    Add Index component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,18 @@
+import Index from "./Index";
+
 require('./bootstrap');
 
 import router from "./routes";
 import VueRouter from 'vue-router';
 
 window.Vue = require('vue');
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 Vue.use(VueRouter);
 
 const app = new Vue({
     el: '#app',
-    router
+    router,
+    components: {
+        "Index": Index
+    },
 });
