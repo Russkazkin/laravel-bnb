92ccdcf752af5df01f4144b0885e969490a5d510
Russkazkin <ruslan@skazkin.su>
2020-06-13 17:42:16 +0300

    Add bookables factory

---
+++ database/factories/BookablesFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Bookable;
+use Faker\Generator as Faker;
+
+$factory->define(Bookable::class, function (Faker $faker) {
+    return [
+        'name' => 'Villa' . $faker->name,
+        'description' => $faker->paragraph(5),
+        'address' => $faker->address,
+        'phone' => $faker->phoneNumber,
+        'email' => $faker->unique()->safeEmail,
+    ];
+});
