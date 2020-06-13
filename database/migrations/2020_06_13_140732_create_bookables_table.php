dc133d8ee777fcd4de79d811a21f2deccff35ee3
Russkazkin <ruslan@skazkin.su>
2020-06-13 17:17:34 +0300

    Complete bookables table migration

--- database/migrations/2020_06_13_140732_create_bookables_table.php
+++ database/migrations/2020_06_13_140732_create_bookables_table.php
@@ -15,6 +15,8 @@ class CreateBookablesTable extends Migration
     {
         Schema::create('bookables', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
             $table->timestamps();
         });
     }
