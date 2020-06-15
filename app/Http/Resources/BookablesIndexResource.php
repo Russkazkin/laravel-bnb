7e0ebc49ffc8d7103bcd66dbce5723ce212410c0
Russkazkin <ruslan@skazkin.su>
2020-06-15 11:01:45 +0300

    Instal laravel-ide-helper

--- app/Http/Resources/BookablesIndexResource.php
+++ app/Http/Resources/BookablesIndexResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Bookable;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class BookablesIndexResource extends JsonResource
@@ -14,6 +15,11 @@ class BookablesIndexResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        /**
+         * @var $this Bookable
+         */
+        return [
+
+        ];
     }
 }
