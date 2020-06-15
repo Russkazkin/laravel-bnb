c3df5d2500b67b43f9fb38474d23b77c7fae1e45
Russkazkin <ruslan@skazkin.su>
2020-06-15 11:02:27 +0300

    Generate phpdocs for out models

--- app/Bookable.php
+++ app/Bookable.php
@@ -4,6 +4,34 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Bookable
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property string $image
+ * @property float $price
+ * @property string $address
+ * @property string $phone
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Bookable newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Bookable newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Bookable query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Bookable whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Bookable whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Bookable whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Bookable whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Bookable whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Bookable whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Bookable whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Bookable wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Bookable wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Bookable whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Bookable extends Model
 {
     //
