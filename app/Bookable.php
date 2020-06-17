242777617a92660f860cc66a7092b2be7fdd1dc1
Russkazkin <ruslan@skazkin.su>
2020-06-17 10:04:56 +0300

    Add models relationship methods

--- app/Bookable.php
+++ app/Bookable.php
@@ -34,5 +34,8 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Bookable extends Model
 {
-    //
+    public function booking()
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
--- app/Booking.php
+++ app/Booking.php
@@ -4,7 +4,30 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Booking
+ *
+ * @property int $id
+ * @property string $from
+ * @property string $to
+ * @property int $bookable_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Booking newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Booking newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Booking query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Booking whereBookableId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Booking whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Booking whereFrom($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Booking whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Booking whereTo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Booking whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Booking extends Model
 {
-    //
+    public function bookable()
+    {
+        return $this->belongsTo(Bookable::class);
+    }
 }
