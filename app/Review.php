cd0033616e57c6419920c0a05c6cc5d9f56d1476
Russkazkin <ruslan@skazkin.su>
2020-06-27 10:08:39 +0300

    Add keyType for Review model

--- app/Review.php
+++ app/Review.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
 }
