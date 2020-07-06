d0a7af8bc466d9c6f701877532f334f577023dbc
Russkazkin <ruslan@skazkin.su>
2020-07-06 10:59:46 +0300

    Add action and ReviewResource for it

--- app/Http/Controllers/Api/ReviewController.php
+++ app/Http/Controllers/Api/ReviewController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ReviewResource;
 use App\Review;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,6 @@ class ReviewController extends Controller
 {
     public function show(Review $review)
     {
-
+        return new ReviewResource($review);
     }
 }
