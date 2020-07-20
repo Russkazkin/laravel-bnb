175613acda1f0e12bd86a3061da378c752ada714
Russkazkin <ruslan@skazkin.su>
2020-07-20 13:08:31 +0300

    Add Success vue component

---
+++ resources/js/shared/components/Success.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="alert alert-success">
+        <slot/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Success"
+    }
+</script>
+
+<style scoped>
+
+</style>
