8d630a2428efdb057a72f5a690e79949e6dc848b
Russkazkin <ruslan@skazkin.su>
2020-07-30 09:39:01 +0300

    Add basket item remove animation

--- resources/js/basket/Basket.vue
+++ resources/js/basket/Basket.vue
@@ -14,26 +14,28 @@
                         <span v-else>Empty</span>
                     </h6>
                 </div>
-                <div v-for="item in basket" :key="item.bookable.id">
-                    <div class="pt-2 pb-2 border-top d-flex justify-content-between">
-                        <div>
-                            <router-link :to="{ name: 'bookable', params: { id: item.bookable.id }}">{{ item.bookable.name }}</router-link>
+                <transition-group name="fade" tag="div">
+                    <div v-for="item in basket" :key="item.bookable.id">
+                        <div class="pt-2 pb-2 border-top d-flex justify-content-between">
+                            <div>
+                                <router-link :to="{ name: 'bookable', params: { id: item.bookable.id }}">{{ item.bookable.name }}</router-link>
+                            </div>
+                            <div class="font-weight-bolder">${{ item.total.total }}.00</div>
                         </div>
-                        <div class="font-weight-bolder">${{ item.total.total }}.00</div>
+                        <div class="pb-2 d-flex justify-content-between text-secondary text-small-3">
+                            <div>From {{ item.dates.from | dateFormatted }}</div>
+                            <div>To {{ item.dates.to | dateFormatted }}</div>
+                        </div>
+                        <div class="pb-3 d-flex justify-content-between text-secondary">
+                            <button
+                                role="button"
+                                class="btn btn-outline-secondary btn-sm btn-block"
+                                @click="$store.dispatch('removeFromBasket', item.bookable.id)">
+                                <i class="fas fa-trash-alt"></i> Remove
+                            </button>
+                        </div>
                     </div>
-                    <div class="pb-2 d-flex justify-content-between text-secondary text-small-3">
-                        <div>From {{ item.dates.from | dateFormatted }}</div>
-                        <div>To {{ item.dates.to | dateFormatted }}</div>
-                    </div>
-                    <div class="pb-3 d-flex justify-content-between text-secondary">
-                        <button
-                            role="button"
-                            class="btn btn-outline-secondary btn-sm btn-block"
-                            @click="$store.dispatch('removeFromBasket', item.bookable.id)">
-                            <i class="fas fa-trash-alt"></i> Remove
-                        </button>
-                    </div>
-                </div>
+                </transition-group>
             </div>
         </div>
     </div>
