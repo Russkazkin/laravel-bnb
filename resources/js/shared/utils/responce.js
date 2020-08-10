45b08fa74910de88d38c6267324e1f7e60c4df83
Russkazkin <ruslan@skazkin.su>
2020-08-10 08:17:46 +0300

    Add minor fixes

--- resources/js/shared/utils/responce.js
+++ resources/js/shared/utils/responce.js
@@ -1,11 +1,11 @@
 export const is404 = function (e) {
-    return isErrorWithResponceAndStatus(e) && e.response.status === 404;
+    return isErrorWithResponseAndStatus(e) && e.response.status === 404;
 }
 
 export const is422 = function (e) {
-    return isErrorWithResponceAndStatus(e) && e.response.status === 422;
+    return isErrorWithResponseAndStatus(e) && e.response.status === 422;
 }
 
-const isErrorWithResponceAndStatus = function(e) {
+const isErrorWithResponseAndStatus = function(e) {
     return e.response && e.response.status;
 }
