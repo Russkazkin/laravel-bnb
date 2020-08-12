66228832b01d8480dd0fb3b7620f41e88ec59587
Russkazkin <ruslan@skazkin.su>
2020-08-12 13:04:16 +0300

    Add users table seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UsersTableSeeder::class);
         $this->call(BookablesSeeder::class);
         $this->call(BookingsSeeder::class);
         $this->call(ReviewsTableSeeder::class);
