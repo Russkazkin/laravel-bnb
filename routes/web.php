167e306784d8d9033f8848da1d4e1479cba077b4
Russkazkin <ruslan@skazkin.su>
2020-08-12 13:20:23 +0300

    Fix routes order

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
 Route::get('/{any?}', function () {
     return view('welcome');
 })->where('any', '^(?!api\/)[\/\w\.-]*');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
